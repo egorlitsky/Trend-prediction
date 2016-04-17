fb6f7ae2a0def6ac67b9970602e7c4fd46b2ac35
egorlitsky <egorlitsky@gmail.com>
2016-04-17 03:28:33 +0300

    UPD Generator interface. Added the degree and exponential generations to artificial generator

--- src/main/java/ru/suai/generators/Generator.java
+++ src/main/java/ru/suai/generators/Generator.java
@@ -4,5 +4,9 @@ package ru.suai.generators;
  * Interface for generating data of different types
  */
 public interface Generator {
-    Object getNextValue(int x);
+    Object getLinearValue(double x);
+
+    Object getDegreeValue(double x);
+
+    Object getExponentialValue(double x);
 }
