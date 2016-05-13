3899fa707fc21332d2f4dad6ce07f34ff4c40c25
egorlitsky <egorlitsky@gmail.com>
2016-05-13 04:42:16 +0300

    Refactor the view

--- src/main/java/ru/suai/view/Visualizator.java
+++ src/main/java/ru/suai/view/Visualizator.java
@@ -4,7 +4,8 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 
 import java.io.*;
@@ -20,9 +21,10 @@ public class Visualizator extends ApplicationFrame {
     public static final String CSV_DELIMITER = "\n";
     public static final String ERROR_IN_SAVING_CSV_FILE = "Error in saving .CSV file";
     public static final String WINDOW_TITLE = "Graphics";
-    public static final String COLUMN_TITLE = "Time";
-    public static final String ROW_TITLE = "Workload";
+    public static final String COLUMN_TITLE = "Time moments";
+    public static final String ROW_TITLE = "Workload value";
 
+    // layout parameters
     public static final int WIDTH_OF_THE_GRAPHIC = 1200;
     public static final int HEIGHT_OF_THE_GRAPHIC = 500;
 
@@ -30,12 +32,25 @@ public class Visualizator extends ApplicationFrame {
     /**
      * Class for visualisation the results of trend-prediction work
      */
-    public Visualizator(DefaultCategoryDataset dataSet) {
+    public Visualizator(XYSeries generated, XYSeries smoothed, XYSeries predicted) {
         super(WINDOW_TITLE);
-        JFreeChart lineChart = ChartFactory.createLineChart("", COLUMN_TITLE, ROW_TITLE,
-                dataSet,
+
+        final XYSeriesCollection data = new XYSeriesCollection();
+
+        data.addSeries(generated);
+        data.addSeries(smoothed);
+        data.addSeries(predicted);
+
+        final JFreeChart lineChart = ChartFactory.createXYLineChart(
+                "Trend prediction demo",
+                COLUMN_TITLE,
+                ROW_TITLE,
+                data,
                 PlotOrientation.VERTICAL,
-                true, true, false);
+                true,
+                true,
+                false
+        );
 
         // chart with generated, smoothed and predicted data
         ChartPanel chartPanel = new ChartPanel(lineChart);
