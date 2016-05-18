3c26fdbfd748d654faf94dc4a0541c2323f8c4ec
egorlitsky <egorlitsky@gmail.com>
2016-05-18 20:52:22 +0300

    Added tests of Predictor function determination with DiurnalGenerator

--- src/test/java/TestPredictor.java
+++ src/test/java/TestPredictor.java
@@ -1,8 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import ru.suai.computing.DataSmoothing;
 import ru.suai.computing.Predictor;
 import ru.suai.generators.ArtificialGenerator;
+import ru.suai.generators.DiurnalGenerator;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertTrue;
 
@@ -149,5 +153,134 @@ public class TestPredictor {
         }
 
         assertTrue((double) this.artificialGenerator.getValue(testElementsCount + 1) == this.predictor.getPredict());
+    }
+
+    @Test
+    public void testLinearFunctionTypeDeterminationByDiurnal() {
+        int w = 15,
+                i = 1,
+                qos = 100,
+                futurePredicts = 3;
+
+        double p = 0.8,
+                currentSmoothValue,
+                generatedNumber;
+
+        HashMap<DiurnalGenerator.modulation, Double> modulation = new HashMap<>();
+        modulation.put(DiurnalGenerator.modulation.AMPLITUDE, 5.0);
+        modulation.put(DiurnalGenerator.modulation.PERIOD, 15.0);
+        modulation.put(DiurnalGenerator.modulation.PHASE, 0.0);
+
+        HashMap<DiurnalGenerator.distribution, String> distribution = new HashMap<>();
+        distribution.put(DiurnalGenerator.distribution.DISTRIBUTION_TYPE, DiurnalGenerator.POISSON_DISTRIBUTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.SHAPE_TYPE, Predictor.LINEAR_FUNCTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_A, "3");
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_B, "2");
+
+        DataSmoothing ds = new DataSmoothing(w, p);
+        this.predictor = new Predictor(w, w, futurePredicts, qos);
+        DiurnalGenerator diurnalGenerator = new DiurnalGenerator(modulation, distribution, 50);
+
+        for (int j = 1; j <= 125; j++) {
+
+            generatedNumber = diurnalGenerator.getValue(i);
+
+            ds.addValue(generatedNumber);
+
+            if (i % w == 0) {
+                currentSmoothValue = ds.getHybridSmoothValue();
+                this.predictor.addValue(currentSmoothValue);
+            }
+            ++i;
+        }
+
+        this.predictor.getPredict();
+        assertTrue(this.predictor.getCurrentFunctionType().equals(Predictor.LINEAR_FUNCTION_TYPE));
+    }
+
+    @Test
+    public void testDegreeFunctionTypeDeterminationByDiurnal() {
+        int w = 15,
+                i = 1,
+                qos = 100,
+                futurePredicts = 3;
+
+        double p = 0.8,
+                currentSmoothValue,
+                generatedNumber;
+
+        HashMap<DiurnalGenerator.modulation, Double> modulation = new HashMap<>();
+        modulation.put(DiurnalGenerator.modulation.AMPLITUDE, 5.0);
+        modulation.put(DiurnalGenerator.modulation.PERIOD, 15.0);
+        modulation.put(DiurnalGenerator.modulation.PHASE, 0.0);
+
+        HashMap<DiurnalGenerator.distribution, String> distribution = new HashMap<>();
+        distribution.put(DiurnalGenerator.distribution.DISTRIBUTION_TYPE, DiurnalGenerator.POISSON_DISTRIBUTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.SHAPE_TYPE, Predictor.DEGREE_FUNCTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_A, "3");
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_B, "4");
+
+        DataSmoothing ds = new DataSmoothing(w, p);
+        this.predictor = new Predictor(w, w, futurePredicts, qos);
+        DiurnalGenerator diurnalGenerator = new DiurnalGenerator(modulation, distribution, 50);
+
+        for (int j = 1; j <= 125; j++) {
+
+            generatedNumber = diurnalGenerator.getValue(i);
+
+            ds.addValue(generatedNumber);
+
+            if (i % w == 0) {
+                currentSmoothValue = ds.getHybridSmoothValue();
+                this.predictor.addValue(currentSmoothValue);
+            }
+            ++i;
+        }
+
+        this.predictor.getPredict();
+        assertTrue(this.predictor.getCurrentFunctionType().equals(Predictor.DEGREE_FUNCTION_TYPE));
+    }
+
+    @Test
+    public void testExponentialFunctionTypeDeterminationByDiurnal() {
+        int w = 15,
+                i = 1,
+                qos = 100,
+                futurePredicts = 3;
+
+        double p = 0.8, // proportion of maximum values for averaging
+                currentSmoothValue,
+                generatedNumber;
+
+        HashMap<DiurnalGenerator.modulation, Double> modulation = new HashMap<>();
+        modulation.put(DiurnalGenerator.modulation.AMPLITUDE, 5.0);
+        modulation.put(DiurnalGenerator.modulation.PERIOD, 15.0);
+        modulation.put(DiurnalGenerator.modulation.PHASE, 0.0);
+
+        HashMap<DiurnalGenerator.distribution, String> distribution = new HashMap<>();
+        distribution.put(DiurnalGenerator.distribution.DISTRIBUTION_TYPE, DiurnalGenerator.POISSON_DISTRIBUTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.SHAPE_TYPE, Predictor.EXPONENTIAL_FUNCTION_TYPE);
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_A, "2");
+        distribution.put(DiurnalGenerator.distribution.COEFFICIENT_B, "4");
+
+        DataSmoothing ds = new DataSmoothing(w, p);
+        this.predictor = new Predictor(w, w, futurePredicts, qos);
+        DiurnalGenerator diurnalGenerator = new DiurnalGenerator(modulation, distribution, 50);
+
+        for (int j = 1; j <= 125; j++) {
+            generatedNumber = diurnalGenerator.getValue(i);
+
+            ds.addValue(generatedNumber);
+
+            if (i % w == 0) {
+                currentSmoothValue = ds.getHybridSmoothValue();
+                this.predictor.addValue(currentSmoothValue);
+            }
+            ++i;
+        }
+
+        this.predictor.getPredict();
+
+        assertTrue(this.predictor.getCurrentFunctionType().equals(Predictor.EXPONENTIAL_FUNCTION_TYPE));
     }
 }
